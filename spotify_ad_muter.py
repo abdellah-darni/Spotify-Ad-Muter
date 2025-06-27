ca0fa6c0e62f91c67802554502f49485517dca31
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-06-27 18:43:38 +0100

    Muting only the Spotify Stream insted of the system stream

--- spotify_ad_muter.py
+++ spotify_ad_muter.py
@@ -8,10 +8,69 @@ POLL_INTERVAL = 1.0
 SPOTIFY_APP_CLASS = "Spotify"
 
 
+def input_to_list_of_dict(input):
+    segments = input.split("Sink Input #")
+
+    list_of_dict = []
+
+    for segment in segments:
+        lines = segment.strip().split("\n")
+
+        if lines and lines[0]:
+            seg_dict = {}
+            propreties_dict = {}
+        
+            seg_dict["Sink Input"] = lines[0].strip()
+
+            for line in lines[1:]:
+                line = line.strip()
+
+                if line.startswith("Properties: "):
+                    continue
+                elif line:
+                    key_value = line.split(":", 1)
+                    if len(key_value) == 2:
+                        seg_dict[key_value[0].strip()] = key_value[1].strip()
+        
+            for line in lines:
+                line = line.strip()
+
+                if line.startswith("Properties: "):
+                    continue
+                elif line:
+                    if "=" in line:
+                        key_value = line.split("=", 1)
+                        if len(key_value) == 2:
+                            propreties_dict[key_value[0].strip()] = key_value[1].strip().strip('"')
+
+            if propreties_dict:
+                seg_dict["Properties"] = propreties_dict
+
+            list_of_dict.append(seg_dict)
+
+    return list_of_dict
+
+            
 
 def set_mute(state: bool):
     flag = '1' if state else '0'
-    subprocess.call(['pactl', 'set-sink-mute', '@DEFAULT_SINK@', flag])
+
+    try :
+        out = subprocess.check_output(['pactl','list','sink-inputs']).decode('utf-8')
+        proc_output = input_to_list_of_dict(out)
+        #print(proc_output)
+        for stream in proc_output:
+            prop = stream['Properties']
+            #print(prop)
+            if prop["application.name"] == "spotify":
+               stream_id = stream['Sink Input']
+               subprocess.call(['pactl', 'set-sink-input-mute', stream_id, flag])
+               print(f"Spotify stream, id : {stream_id}")
+    except Exception as e:
+        subprocess.call(['pactl', 'set-sink-mute', '@DEFAULT_SINK@', flag])
+        print(f"failed to detect Spotify stream, switching to System stream: {e}")
+           
+    
 
 
 def get_spotify_window_title():
@@ -44,7 +103,7 @@ def main():
     while True:
         ad = is_ad_playing()
         current_title = get_spotify_window_title()
-        print(f"Current Playing: {current_title}")
+        #print(f"Current Playing: {current_title}")
         if ad and last_played_is_muted is not True:
             print(f"Ad detected (title: {current_title}), muting audio.")
             set_mute(True)
